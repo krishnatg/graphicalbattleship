c0a66c6ee97ec0876454d8fc54b09a8de95e7f77
Krishna Tippur Gururaj <krishnatg@gmail.com>
2017-10-11 15:41:08 -0400

    added startup of rmiregistry via code

--- Server.java
+++ Server.java
@@ -7,10 +7,20 @@ import java.rmi.Naming;
  */
 public class Server {
     public static void main(String args[]) {
+        int port = 30097 ; // can be changed as needed
+        try {
+            LocateRegistry.createRegistry(port) ;
+        }
+        catch (RemoteException e) {
+            e.printStackTrace() ;
+            System.err.println("unable to start RMI registry on " + port) ;
+            System.exit(1) ;
+        }
         System.out.println("Battleship game started - Server side") ;
         try {
+            String str = "//localhost:" + port + "/BattleshipObj" ;
             Game g = new Game() ;
-            Naming.rebind("//localhost:30097/BattleshipObj", g) ;
+            Naming.rebind(str, g) ;
             System.out.println("Bound in registry") ;
         }
         catch (Exception e) {
